d62621f44112ad819981b790725fa910b1b7d62a
PedroLavarda <plzorel@gmail.com>
2024-11-18 19:19:41 -0300

    products websecurity + remove post get users from websecurity

--- src/main/java/br/edu/projeto/server/service/impl/CategoryServiceImpl.java
+++ src/main/java/br/edu/projeto/server/service/impl/CategoryServiceImpl.java
@@ -3,13 +3,8 @@ package br.edu.projeto.server.service.impl;
 import br.edu.projeto.server.model.Category;
 import br.edu.projeto.server.repository.CategoryRepository;
 import br.edu.projeto.server.service.ICategoryService;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class CategoryServiceImpl extends CrudServiceImpl<Category, Long> implements ICategoryService {
