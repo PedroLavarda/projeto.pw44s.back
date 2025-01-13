ae8fc6272c74ba7499ea97765413012d994de628
PedroLavarda <plzorel@gmail.com>
2025-01-13 13:07:13 -0300

    feat: added more attributes to user dto for frontend

--- src/main/java/br/edu/projeto/server/security/dto/UserResponseDTO.java
+++ src/main/java/br/edu/projeto/server/security/dto/UserResponseDTO.java
@@ -7,6 +7,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 
+import java.sql.Date;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -15,14 +16,25 @@ import java.util.Set;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserResponseDTO {
-
-    private String displayName;
+    private Long id;
     private String username;
+    private String displayName;
+    private String cpf;
+    private String rg;
+    private Date birthDate;
+    private String email;
+    private String phone;
     private Set<AuthorityResponseDTO> authorities;
 
     public UserResponseDTO(User user) {
+        this.id = user.getId();
         this.displayName = user.getDisplayName();
         this.username = user.getUsername();
+        this.cpf = user.getCpf();
+        this.rg = user.getRg();
+        this.birthDate = user.getBirthDate();
+        this.email = user.getEmail();
+        this.phone = user.getPhone();
         this.authorities = new HashSet<>();
         for (GrantedAuthority authority: user.getAuthorities()) {
             authorities.add( new AuthorityResponseDTO(authority.getAuthority()) );
